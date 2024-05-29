632b99f7ca049d3eea8d473b3bcc0f1032171ac4
RR <124536741+jhamer8@users.noreply.github.com>
2024-05-28 21:06:51 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,19 @@ export default function Home() {
     <div className="flex items-center justify-center h-screen">
       <div className="flex flex-col items-center justify-center text-8xl">
         <h1 className="text-yellow-500">Welcome to my</h1>
-        <FlipWords words={["personal website", "digital portfolio", "creative outlet", "exploration hub"]} />
+        <FlipWords
+          words={[
+            "personal website",
+            "tech journey",
+            "code chronicles",
+            "tech adventures",
+            "coding odyssey",
+            "tech explorations",
+            "tech portfolio",
+            "cs projects",
+            "digital showcase",
+          ]}
+        />
       </div>
     </div>
   );
