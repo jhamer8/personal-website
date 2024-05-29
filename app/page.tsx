b6a43b1f7c65d828680828a860b681ad79d537fd
RR <124536741+jhamer8@users.noreply.github.com>
2024-05-28 21:16:10 -0700

    Responsive text sizing

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,23 @@
 import Image from "next/image";
 import { FlipWords } from "@/components/ui/flip-words";
-
+const suffix = [
+  "personal website",
+  "tech journey",
+  "code chronicles",
+  "tech adventures",
+  "coding odyssey",
+  "tech explorations",
+  "tech portfolio",
+  "CS projects",
+  "digital showcase",
+]
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="flex flex-col items-center justify-center text-8xl">
+      <div className="flex flex-col items-center justify-center text-4xl md:text-8xl">
         <h1 className="text-yellow-500">Welcome to my</h1>
         <FlipWords
-          words={[
-            "personal website",
-            "tech journey",
-            "code chronicles",
-            "tech adventures",
-            "coding odyssey",
-            "tech explorations",
-            "tech portfolio",
-            "cs projects",
-            "digital showcase",
-          ]}
+          words={suffix}
         />
       </div>
     </div>
