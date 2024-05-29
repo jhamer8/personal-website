a97f4d6bd3a9fe26a11a1438066ac922a310e75d
RR <124536741+jhamer8@users.noreply.github.com>
2024-05-28 21:03:46 -0700

    Bug fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,13 @@
 import React from "react";
 import { motion, useAnimation } from "framer-motion";
 
-function Header() {
+const useNavItemAnimation = () => {
+  return useAnimation();
+};
+
+const NavItem = ({ text, href }: { text: string; href: string }) => {
+  const controls = useNavItemAnimation();
+
   const motionProps = {
     whileHover: { scale: 1.15 },
     transition: { type: "spring", stiffness: 90 },
@@ -18,39 +24,37 @@ function Header() {
     visible: { width: "100%" },
   };
 
-  const createNavItem = (text: string, href: string) => {
-    const controls = useAnimation();
+  return (
+    <motion.a
+      {...motionProps}
+      href={href}
+      onHoverStart={() => controls.start("visible")}
+      onHoverEnd={() => controls.start("hidden")}
+    >
+      <motion.div className="relative">
+        <motion.h1 {...textHoverProps} className="text-white">
+          {text}
+        </motion.h1>
+        <motion.div
+          className="absolute bottom-0 left-0 h-0.5 bg-yellow-500"
+          variants={underlineVariants}
+          initial="hidden"
+          animate={controls}
+          transition={{ duration: 0.3 }}
+        />
+      </motion.div>
+    </motion.a>
+  );
+};
 
-    return (
-      <motion.a
-        {...motionProps}
-        href={href}
-        onHoverStart={() => controls.start("visible")}
-        onHoverEnd={() => controls.start("hidden")}
-      >
-        <motion.div className="relative">
-          <motion.h1 {...textHoverProps} className="text-white">
-            {text}
-          </motion.h1>
-          <motion.div
-            className="absolute bottom-0 left-0 h-0.5 bg-yellow-500"
-            variants={underlineVariants}
-            initial="hidden"
-            animate={controls}
-            transition={{ duration: 0.3 }}
-          />
-        </motion.div>
-      </motion.a>
-    );
-  };
-
+function Header() {
   return (
     <header className="z-20">
       <div className="flex flex-1 justify-center space-x-12 text-lg mt-8">
-        {createNavItem("HOME", "/")}
-        {createNavItem("ABOUT", "/about")}
-        {createNavItem("SKILLS", "/skills")}
-        {createNavItem("CONTACT", "/contact")}
+        <NavItem text="HOME" href="/" />
+        <NavItem text="ABOUT" href="/about" />
+        <NavItem text="SKILLS" href="/skills" />
+        <NavItem text="CONTACT" href="/contact" />
       </div>
     </header>
   );
