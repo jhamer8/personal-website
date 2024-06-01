c53f12397e9f7c7944efa08e87dd844f017b0e95
RR <124536741+jhamer8@users.noreply.github.com>
2024-05-31 21:14:07 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { FlipWords } from "@/components/ui/flip-words";
 import { Rock_Salt } from "next/font/google";
 import { useState, useEffect } from "react";
 import { motion, useMotionValue, useTransform, animate } from "framer-motion";
-import { BeakerIcon, PaperAirplaneIcon } from '@heroicons/react/24/solid';
+import { BeakerIcon } from '@heroicons/react/24/solid';
 import { useFlubber } from "./hooks/use-flubber";
 
 const rockSalt = Rock_Salt({
@@ -25,7 +25,7 @@ const suffix = [
 ];
 
 const beakerPath = "M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"; // BeakerIcon path
-const planePath = "M2.5 19.5l19-7-19-7v6l14 1-14 1v6z"; // PaperAirplaneIcon path
+const planePath = "M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"; // PaperAirplaneIcon path
 
 export default function Home() {
   const [isHovering, setIsHovering] = useState(false);
@@ -74,4 +74,5 @@ export default function Home() {
     </div>
   );
 }
+
 
