e41854e3f6f3965c395ff249e6c500fcd7ae1adc
RR <124536741+jhamer8@users.noreply.github.com>
2024-06-02 12:48:24 -0700

    Clean mobile navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,11 +2,16 @@
 import React from "react";
 import NavItem from "./ui/nav-item";
 import { Bars3Icon } from "@heroicons/react/24/outline";
+import { Button } from "@/components/ui/button";
+
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 function Header() {
   return (
@@ -18,21 +23,29 @@ function Header() {
         <NavItem text="CONTACT" href="/contact" />
       </div>
       <div className="flex ml-8 justify-start mt-8 sm:hidden w-full">
-        <Popover>
-          <PopoverTrigger>
+        <Sheet>
+          <SheetTrigger>
             <Bars3Icon className="h-8 w-8 text-white" />
-          </PopoverTrigger>
-          <PopoverContent className="flex flex-col justify-start text-themeOrange ">
-            <a className="" href="/">HOME</a>
-            <a className="" href="/about">ABOUT</a>
-            <a className="" href="/skills">SKILLS</a>
-            <a className="" href="/contact">CONTACT</a>
-          </PopoverContent>
-        </Popover>
+          </SheetTrigger>
+          <SheetContent side={"top"} className="flex flex-col justify-start text-themePurple">
+            {/* <SheetHeader>
+              <SheetTitle>Are you absolutely sure?</SheetTitle>
+              <SheetDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </SheetDescription>
+            </SheetHeader> */}
+            <nav className="flex flex-col space-y-4">
+              <a href="/" className="text-lg font-medium hover:text-themeOrange">Home</a>
+              <a href="/about" className="text-lg font-medium hover:text-themeOrange">About</a>
+              <a href="/skills" className="text-lg font-medium hover:text-themeOrange">Skills</a>
+              <a href="/contact" className="text-lg font-medium hover:text-themeOrange">Contact</a>
+            </nav>
+          </SheetContent>
+        </Sheet>
       </div>
     </header>
   );
 }
 
 export default Header;
-
