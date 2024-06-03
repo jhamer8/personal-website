15cc42677cc900f40f04107d5be5d07e9b5880e8
RR <124536741+jhamer8@users.noreply.github.com>
2024-06-02 17:33:40 -0700

    Skeleton loader for the map

--- components/Map.tsx
+++ components/Map.tsx
@@ -3,15 +3,20 @@ import * as React from 'react';
 import Head from 'next/head';
 import Map, { Marker } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { Skeleton } from "@/components/ui/skeleton"
+
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoidGh1dHplbGwiLCJhIjoiY2x3eTRiZmI0MWsxNzJqcHN6OW1oZGhvZiJ9.ZHoWaQzmMHi0y4YkknnfDw';
 
 export default function MapComponent() {
+  const [loading, setLoading] = React.useState(true);
+
   return (
     <div className="relative w-full h-80 sm:h-[500px] rounded-lg shadow-lg overflow-hidden max-w-lg border-2 border-gray-300 dark:border-gray-700">
       <Head>
         <title>Map</title>
       </Head>
+      {loading && <Skeleton className="absolute inset-0 animate-pulse bg-gradient-to-r from-gray-300 via-gray-200 to-gray-300" />}
       <Map
         initialViewState={{
           latitude: 33.7773732945584,
@@ -21,9 +26,11 @@ export default function MapComponent() {
         style={{ width: '100%', height: '100%' }}
         mapStyle="mapbox://styles/mapbox/navigation-night-v1"
         mapboxAccessToken={MAPBOX_TOKEN}
+        onLoad={() => setLoading(false)}
       >
         <Marker longitude={-84.39607426503417} latitude={33.7773732945584} color="red" />
       </Map>
     </div>
   );
 }
+
